abfba7eff5f5a91ebbbec91ab5f56d20799317c8
Yanbang Jia <yanbangjia@gmail.com>
2025-07-19 20:30:46 +0800

    java basic skills update.

--- learningjavaproject/src/learningjavaproject/JavaBasic.java
+++ learningjavaproject/src/learningjavaproject/JavaBasic.java
@@ -1,5 +1,10 @@
 package learningjavaproject;
 
 public class JavaBasic {
-
+	public static void main(String[] args) {
+        String[] names = {"ABC", "XYZ", "zoo"};
+        String s = names[1];
+        names[1] = "cat";
+        System.out.println(s); // s是"XYZ"还是"cat"?
+	}
 }
