f3a094fd2de16c96432bc66734ec8b888c882080
Yanbang Jia <yanbangjia@gmail.com>
2025-07-21 23:06:43 +0800

    java basic update.

--- learningjavaproject/src/learningjavaproject/JavaBasic.java
+++ learningjavaproject/src/learningjavaproject/JavaBasic.java
@@ -1,10 +1,15 @@
 package learningjavaproject;
+import java.util.Scanner;
 
 public class JavaBasic {
-	public static void main(String[] args) {
-        String[] names = {"ABC", "XYZ", "zoo"};
-        String s = names[1];
-        names[1] = "cat";
-        System.out.println(s); // s是"XYZ"还是"cat"?
+	public static void main(String[] args) {	
+		var scanner = new Scanner(System.in);
+		System.out.print("Input Xiaoming's last score: ");
+		int last_score = scanner.nextInt();
+		System.out.print("Input Xiaoming's current score: ");
+        int current_score = scanner.nextInt();
+        double raise_score = (double)(current_score - last_score) / last_score * 100;
+        System.out.printf("\n Xiaoming's score raised: %.2f%%", raise_score);
+        scanner.close();
 	}
 }
