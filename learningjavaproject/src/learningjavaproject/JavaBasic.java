461e5f2140a19baf8386135c714311f856971173
Yanbang Jia <yanbangjia@gmail.com>
2025-07-23 22:53:16 +0800

    java basic skills update.

--- learningjavaproject/src/learningjavaproject/JavaBasic.java
+++ learningjavaproject/src/learningjavaproject/JavaBasic.java
@@ -1,23 +1,6 @@
+// xxxxx
 package learningjavaproject;
-//import java.util.Arrays;
 
 public class JavaBasic {
-	static String name;
 	
-	public static void main(String[] args) {
-		String x = "hello";
-		setName(x);
-		getName();
-		x = "world";
-		getName();
-            
-	}
-	
-    public static void setName(String namesx) {
-        name = namesx;
-    }
-    
-    public static void getName() {
-        System.out.println(name);
-    }
 }
